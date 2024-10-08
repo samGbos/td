18e744dc79ec42c3d5211263188c17313dd9182e
Makimadness <makimadness7@gmail.com>
2024-10-07 22:18:15 -0400

    well i was going to make the turret point to mouse pointer but IDK how to make it point toward the mouse... SAM HELP MEEEEE

---
+++ TurretDesignKoji.py
@@ -0,0 +1,14 @@
+import pygame
+import math
+import time
+
+pygame.init()
+width = 800
+height = 600
+screen = pygame.display.set_mode([width, height])
+clock = pygame.time.Clock()
+run = True
+while run:
+    keys = pygame.key.get_pressed()
+    #atueaert ,.erfgv ,gv ,vgk,.r sam please help me im lost
+    clock.tick(165)
